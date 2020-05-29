ae93e90b486c5e6811abe4a09306c7b3f892c4db
Glaucio Guerra <glaucioguerra@gmail.com>
2020-05-29 12:31:56 -0300

    adicionado novo teste de integracao

--- testes-integracao.sh
+++ testes-integracao.sh
@@ -3,3 +3,4 @@ curl http://8090/api/notes
 curl -H "Content-Type: application/json" --data @note.json http://localhost:8090/api/notes
 curl http://localhost:8090/api/notes
 curl -X DELETE -H "Content-Type: application/json" http://localhost:8090/api/notes/1
+curl http://localhost:8090/api/notes
