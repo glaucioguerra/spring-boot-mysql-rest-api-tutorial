8369ee7904def3b52cb34509c3b3ea3b20ae89f5
Glaucio Guerra <glaucioguerra@gmail.com>
2020-05-31 23:18:49 -0300

    ajuste teste integracao

--- testes-integracao.sh
+++ testes-integracao.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-curl http://8090/api/notes
+curl http://localhost:8090/api/notes
 curl -H "Content-Type: application/json" --data @note.json http://localhost:8090/api/notes
 curl http://localhost:8090/api/notes
 curl -X DELETE -H "Content-Type: application/json" http://localhost:8090/api/notes/1
